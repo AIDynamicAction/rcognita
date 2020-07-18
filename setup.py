b413bd4e08708cf3ff86ee3a7675312bd53a1025
Eli Bolotin <42818860+ebolotin6@users.noreply.github.com>
2020-07-17 21:55:37 -0700

    Misc changes to setup

--- setup.py
+++ setup.py
@@ -5,31 +5,38 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LearnRLSK", # Replace with your own username
-    version="1.0.2",
+    version="1.0.9",
     author="Pavel Osinenko",
     author_email="p.osinenko@skoltech.ru",
     description="Learning Reinforcement Learning - Skoltech",
     long_description="See homepage",
     long_description_content_type="text/markdown",
-    url="https://github.com/OsinenkoP/learnRL-py",
+    url="https://github.com/OsinenkoP/LearnRLSK",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires="~=3.7",
     install_requires=[
         "numpy",
         "scipy",
+        "matplotlib",
         "control",
         "slycot",
+        "bleach",
+        "docutils",
+        "Pygments",
         "future",
         "tabulate",
         "mpldatacursor",
         "svgpath2mpl",
         "nbconvert",
         "cmake",
-        "scikit-build"],
-    dependency_links=['https://github.com/CPCLAB-UNIPI/SIPPY/tarball/master#egg=SIPPY']
+        "scikit-build",
+        "colorama",
+        "pkginfo",
+        "requests",
+        "tqdm"],
 )
