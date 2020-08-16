c49405cf1f62a745e211a8e1fea947ccc76047e9
Eli Bolotin <42818860+ebolotin6@users.noreply.github.com>
2020-08-16 12:27:10 -0700

    Multiple changes
    - Removed file
    - Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LearnRLSK", # Replace with your own username
-    version="1.1.0",
+    version="1.5.0",
     author="Pavel Osinenko",
     author_email="p.osinenko@skoltech.ru",
     description="Learning Reinforcement Learning - Skoltech",
