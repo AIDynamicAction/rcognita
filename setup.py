079b5697a657f43d27bfaa41b900a2b6c17b81a8
Eli Bolotin <42818860+ebolotin6@users.noreply.github.com>
2020-08-16 19:36:45 -0700

    Renamed dirs

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="LearnRLSK", # Replace with your own username
-    version="1.5.0",
+    name="learnRL-py", # Replace with your own username
+    version="1.6.0",
     author="Pavel Osinenko",
     author_email="p.osinenko@skoltech.ru",
-    description="Learning Reinforcement Learning - Skoltech",
+    description="learnRL-py",
     long_description="See homepage",
     long_description_content_type="text/markdown",
-    url="https://github.com/OsinenkoP/LearnRLSK",
-    packages=['LearnRLSK'],
+    url="https://github.com/AIDA-Skoltech/learnRL-py",
+    packages=['learnRL-py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,7 +20,7 @@ setuptools.setup(
     ],
     entry_points={
         'console_scripts': [
-            'lrl = LearnRLSK.__main__:main',
+            'lrl = learnRL-py.__main__:main',
         ],
     },
     python_requires="~=3.7",
