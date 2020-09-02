d79300675c39b223f38ac9cdafae07a14a9b109d
Eli Bolotin <42818860+ebolotin6@users.noreply.github.com>
2020-09-01 19:09:44 -0700

    Working on recursive autograder

--- rcognita/grading_utilities.py
+++ rcognita/grading_utilities.py
@@ -3,7 +3,7 @@ import json
 class AnswerTracker:
 	def __init__(self):
 		self.answers_dict = {}
-
+	
 	def record(self, problem, answer):
 		self.answers_dict[problem] = answer
 
@@ -17,6 +17,22 @@ class AnswerTracker:
 	def print_answers(self):
 		print(self.answers_dict)
 
-	def save_to_json(self):
-		with open('answers.json', 'w') as answers_file:
+	def save_to_json(self, assignment_name, first_name, last_name):
+		file_n = f"{last_name}_{first_name}_{assignment_name}"
+
+		with open(file_n+'_answers.json', 'w') as answers_file:
 			json.dump(self.answers_dict, answers_file)
+
+	# @classmethod
+	# def grade_assignment(cls, answer_key, student_version):
+	# 	files = [answer_key, student_version]
+	# 	decoded_files = []
+		
+	# 	for file in files:
+	# 		with open(file, 'r') as json_file:
+	# 			decoded = json.load(json_file)
+	# 			decoded_files.append(decoded)
+
+	# 	for answers_dict, student_dict in decoded_files:
+			
+			
