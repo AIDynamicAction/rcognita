08417871de2f0a201a1d4fbee7bfedc505a9b8a1
Ilya Osokin <kefir8888@gmail.com>
2022-04-08 20:19:48 +0300

    Added up-to_date estimator from asgn-1

--- rcognita/estimators.py
+++ rcognita/estimators.py
@@ -16,17 +16,22 @@ import torch.nn as nn
 
 import rcognita.models as models
 
-class Estimator_RNN:
+
+class EstimatorRNN:
     """
     Class of model estimators based on recurrent neural networks
     """
 
-    def __init__(self, dim_observation, dim_action, dim_hidden, buffer_size, model = None, Nbackprops = 1):
+    def __init__(self, dim_observation, dim_action, dim_hidden, buffer_size,
+                 model=None, Nbackprops=1, t0=0, sampling_time=0.1):
         self.buffer_size = buffer_size
 
         self.dim_observation = dim_observation
-        self.dim_action      = dim_action
-        self.dim_hidden      = dim_hidden
+        self.dim_action = dim_action
+        self.dim_hidden = dim_hidden
+
+        self.ctrl_clock = t0
+        self.sampling_time = sampling_time
 
         if (model is None):
             self.model = models.ModelRNN(None, self.dim_observation, self.dim_action, self.dim_hidden)
@@ -34,28 +39,48 @@ class Estimator_RNN:
         else:
             self.model = model
 
-        self.criterion = nn.MSELoss()
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        self.criterion = nn.L1Loss()
+        # self.criterion = nn.MSELoss()
 
-        self.observation_buffer = np.zeros((self.buffer_size, self.dim_observation))
-        self.action_buffer      = np.zeros((self.buffer_size, self.dim_action))
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.0005, momentum=0.9)
+
+        self.observation_buffer = np.zeros((self.buffer_size, self.dim_observation), dtype=np.float64)
+        self.action_buffer = np.zeros((self.buffer_size, self.dim_action), dtype=np.float64)
 
         self.Nbackprops = Nbackprops
 
-    def receive_sys_IO(self, observation, action):
-        # push observation, action to buffers -- see functionality in controllers.py, line 1463
+    def receive_sys_IO(self, t, observation, action):
+        time_in_sample = t - self.ctrl_clock
 
-        self.observation_buffer = push_vec(self.observation_buffer, observation)
-        self.action_buffer      = push_vec(self.action_buffer, action)
+        if time_in_sample >= self.sampling_time:  # New sample
+            self.ctrl_clock = t
+
+            self.observation_buffer = push_vec(self.observation_buffer, observation)
+            self.action_buffer = push_vec(self.action_buffer, action)
+
+            return True
+
+        return False
 
     def update_params(self):
         """
         Update neural network weights
         """
 
-        # Torch backprop (Nbackprops times, say) on loss = model accuracy over buffers
+        #############################################
+        # YOUR CODE BELOW
+        #############################################
 
         self.loss.backward()
+        nn.utils.clip_grad_value_(self.model.parameters(), clip_value=1.0)
+        self.optimizer.step()
+
+        #############################################
+        # YOUR CODE ABOVE
+        #############################################
+
+    def get_last_pred(self):
+        return self.last_pred
 
     def output_loss(self):
         """
@@ -63,12 +88,26 @@ class Estimator_RNN:
         """
 
         self.loss = 0
+        self.optimizer.zero_grad()
+
+        #############################################
+        # YOUR CODE BELOW
+        #############################################
 
         for i in range(self.buffer_size - 1):
-            #y_pred = self.model.model_out(np.concatenate((self.observation_buffer[i, :], self.action_buffer[i, :])))
-            y_pred = self.model.model_out(self.observation_buffer[i, :], self.action_buffer[i, :])
+            y_pred = self.model.model_out(self.observation_buffer[i, :],
+                                          self.action_buffer[i, :])
 
-            #loss += np.linalg.norm((y_pred.detach().numpy() - self.observation_buffer[i + 1, :]))
-            self.loss += self.criterion(y_pred, torch.tensor(self.observation_buffer[i + 1, :]))
+            self.loss += self.criterion(y_pred, torch.tensor
+            (self.observation_buffer[i + 1, :]))
+
+            self.last_pred = y_pred
+
+        print("observation_buffer")
+        print(self.observation_buffer)
+
+        #############################################
+        # YOUR CODE ABOVE
+        #############################################
 
         return self.loss.detach().numpy()
