8678f9ff661f581b70a3bd343dc76fa73214d66c
Georgiy Malaniya <pwlsd.gm@gmail.com>
2022-06-21 16:14:32 +0300

    all current presets are covered with tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,6 +3,7 @@ import pickle5 as pickle
 from unittest_data_generator import (
     PresetPipeline3WRobotTest,
     PresetPipeline3WRobotNITest,
+    PresetPipeline2TankTest,
 )
 import pytest
 from numpy.testing import assert_allclose
@@ -34,6 +35,25 @@ class TestPresetPipeline3WRobotNI(PresetPipeline3WRobotNITest):
     @pytest.fixture(autouse=True)
     def load_trajectory(self):
         with open("./refs/trajectory_3wrobot_ni.pickle", "rb") as f:
+            self.reference_trajectory = pickle.load(f)
+
+    def test_run_animate(self):
+        self.pipeline_execution()
+
+    def test_run_raw(self):
+        args = {"is_visualization": False}
+        self.pipeline_execution(args)
+
+    def test_trajectory_(self, load_trajectory):
+        args = {"is_visualization": False}
+        self.pipeline_execution(args, save_trajectory=True)
+        assert_allclose(self.trajectory, self.reference_trajectory)
+
+
+class TestPresetPipeline2Tank(PresetPipeline2TankTest):
+    @pytest.fixture(autouse=True)
+    def load_trajectory(self):
+        with open("./refs/trajectory_2tank.pickle", "rb") as f:
             self.reference_trajectory = pickle.load(f)
 
     def test_run_animate(self):
--- tests/unittest_data_generator.py
+++ tests/unittest_data_generator.py
@@ -1,8 +1,13 @@
 import os, sys
 import pickle5 as pickle
 import matplotlib.animation as animation
+import numpy as np
 import matplotlib.pyplot as plt
-from configs import *  #### we will always import all configs anyway
+from configs import (
+    config_3wrobot,
+    config_3wrobot_ni,
+    config_2tank,
+)
 from abc import ABCMeta, abstractmethod
 
 PARENT_DIR = os.path.abspath(__file__ + "/../..")
@@ -14,7 +19,7 @@ from rcognita.utilities import on_key_press
 
 def config_to_pickle(preset_name):
     with open(f"./refs/env_{preset_name}.pickle", "wb") as env_description_out:
-        pickle.dump(config_3wrobot(), env_description_out)
+        pickle.dump(eval(f"config_{preset_name}()"), env_description_out)
 
 
 def get_env(preset_name):
@@ -483,7 +488,7 @@ class PresetPipeline3WRobotNITest(AbstractPresetPipeline):
         self.my_logger = loggers.Logger3WRobotNI()
 
     def main_loop_visual(self):
-        self.state_full_init = self.my_simulator.state_full
+        state_full_init = self.my_simulator.state_full
 
         my_animator = visuals.Animator3WRobotNI(
             objects=(
@@ -500,17 +505,17 @@ class PresetPipeline3WRobotNITest(AbstractPresetPipeline):
                 self.action_init,
                 self.t0,
                 self.t1,
-                self.state_full_init,
+                state_full_init,
                 self.xMin,
                 self.xMax,
                 self.yMin,
                 self.yMax,
                 self.ctrl_mode,
                 self.action_manual,
-                self.Fmin,
-                self.Mmin,
-                self.Fmax,
-                self.Mmax,
+                self.v_min,
+                self.omega_min,
+                self.v_max,
+                self.omega_max,
                 self.Nruns,
                 self.is_print_sim_step,
                 self.is_log_data,
@@ -535,6 +540,7 @@ class PresetPipeline3WRobotNITest(AbstractPresetPipeline):
         )
 
         anm.running = True
+
         my_animator.fig_sim.tight_layout()
 
         plt.show()
@@ -629,7 +635,230 @@ class PresetPipeline3WRobotNITest(AbstractPresetPipeline):
             pickle.dump(self.trajectory, trajectory)
 
 
+class PresetPipeline2TankTest(AbstractPresetPipeline):
+    def setup_env(self, args={}):
+        self.__dict__.update(vars(get_env("2tank")))
+        self.__dict__.update(args)
+        print(self.__dict__)
+        self.trajectory = []
+
+    def system_initialization(self):
+        self.my_sys = systems.Sys2Tank(
+            sys_type="diff_eqn",
+            dim_state=self.dim_state,
+            dim_input=self.dim_input,
+            dim_output=self.dim_output,
+            dim_disturb=self.dim_disturb,
+            pars=[self.tau1, self.tau2, self.K1, self.K2, self.K3],
+            ctrl_bnds=self.ctrl_bnds,
+        )
+
+    def state_predictor_initialization(self):
+        self.state_predictor = controllers.EulerStatePredictor(
+            self.pred_step_size,
+            self.my_sys._state_dyn,
+            self.my_sys.out,
+            self.dim_output,
+            self.Nactor,
+        )
+
+    def controller_initialization(self):
+        self.my_ctrl_opt_pred = controllers.CtrlOptPred(
+            self.dim_input,
+            self.dim_output,
+            self.ctrl_mode,
+            ctrl_bnds=self.ctrl_bnds,
+            action_init=self.action_init,
+            t0=self.t0,
+            sampling_time=self.dt,
+            Nactor=self.Nactor,
+            pred_step_size=self.pred_step_size,
+            sys_rhs=self.my_sys._state_dyn,
+            sys_out=self.my_sys.out,
+            state_sys=self.state_init,
+            state_predictor=self.state_predictor,
+            prob_noise_pow=self.prob_noise_pow,
+            is_est_model=self.is_est_model,
+            model_est_stage=self.model_est_stage,
+            model_est_period=self.model_est_period,
+            buffer_size=self.buffer_size,
+            model_order=self.model_order,
+            model_est_checks=self.model_est_checks,
+            gamma=self.gamma,
+            Ncritic=self.Ncritic,
+            critic_period=self.critic_period,
+            critic_struct=self.critic_struct,
+            stage_obj_struct=self.stage_obj_struct,
+            stage_obj_pars=[self.R1],
+            observation_target=self.observation_target,
+        )
+
+        self.my_ctrl_benchm = self.my_ctrl_opt_pred
+
+    def simulator_initialization(self):
+        self.my_simulator = simulator.Simulator(
+            sys_type="diff_eqn",
+            closed_loop_rhs=self.my_sys.closed_loop_rhs,
+            sys_out=self.my_sys.out,
+            state_init=self.state_init,
+            disturb_init=[],
+            action_init=self.action_init,
+            t0=self.t0,
+            t1=self.t1,
+            dt=self.dt,
+            max_step=self.dt / 2,
+            first_step=1e-6,
+            atol=self.atol,
+            rtol=self.rtol,
+            is_disturb=self.is_disturb,
+            is_dyn_ctrl=self.is_dyn_ctrl,
+        )
+
+    def logger_initialization(self):
+        self.datafiles = [None] * self.Nruns
+        self.my_logger = loggers.Logger2Tank()
+
+    def main_loop_visual(self):
+        self.state_full_init = self.my_simulator.state_full
+
+        my_animator = visuals.Animator2Tank(
+            objects=(
+                self.my_simulator,
+                self.my_sys,
+                [],
+                self.my_ctrl_benchm,
+                self.datafiles,
+                controllers.ctrl_selector,
+                self.my_logger,
+            ),
+            pars=(
+                self.state_init,
+                self.action_init,
+                self.t0,
+                self.t1,
+                self.state_full_init,
+                self.ctrl_mode,
+                self.action_manual,
+                self.action_min,
+                self.action_max,
+                self.Nruns,
+                self.is_print_sim_step,
+                self.is_log_data,
+                0,
+                [],
+                self.observation_target,
+            ),
+        )
+
+        anm = animation.FuncAnimation(
+            my_animator.fig_sim,
+            my_animator.animate,
+            init_func=my_animator.init_anim,
+            blit=False,
+            interval=self.dt / 1e6,
+            repeat=False,
+        )
+
+        my_animator.get_anm(anm)
+
+        cId = my_animator.fig_sim.canvas.mpl_connect(
+            "key_press_event", lambda event: on_key_press(event, anm)
+        )
+
+        anm.running = True
+
+        my_animator.fig_sim.tight_layout()
+
+        plt.show()
+
+    def main_loop_raw(self, save_trajectory):
+        run_curr = 1
+        datafile = self.datafiles[0]
+
+        while True:
+            self.my_simulator.sim_step()
+
+            t, state, observation, state_full = self.my_simulator.get_sim_step_data()
+
+            action = controllers.ctrl_selector(
+                t,
+                observation,
+                self.action_manual,
+                [],
+                self.my_ctrl_benchm,
+                self.ctrl_mode,
+            )
+
+            self.my_sys.receive_action(action)
+            self.my_ctrl_benchm.receive_sys_state(self.my_sys._state)
+            self.my_ctrl_benchm.upd_accum_obj(observation, action)
+
+            h1 = state_full[0]
+            h2 = state_full[1]
+            p = action
+
+            stage_obj = self.my_ctrl_benchm.stage_obj(observation, action)
+            accum_obj = self.my_ctrl_benchm.accum_obj_val
+
+            if self.is_print_sim_step:
+                self.my_logger.print_sim_step(t, h1, h2, p, stage_obj, accum_obj)
+
+            if self.is_log_data:
+                self.my_logger.log_data_row(
+                    datafile, t, h1, h2, p, stage_obj, accum_obj
+                )
+
+            if t >= self.t1:
+                if self.is_print_sim_step:
+                    print(
+                        ".....................................Run {run:2d} done.....................................".format(
+                            run=run_curr
+                        )
+                    )
+
+                run_curr += 1
+
+                if run_curr > self.Nruns:
+                    break
+
+                if self.is_log_data:
+                    datafile = self.datafiles[run_curr - 1]
+
+                # Reset simulator
+                self.my_simulator.status = "running"
+                self.my_simulator.t = self.t0
+                self.my_simulator.observation = self.state_full_init
+
+                if self.ctrl_mode != "nominal":
+                    self.my_ctrl_benchm.reset(self.t0)
+                else:
+                    self.my_ctrl_nominal.reset(self.t0)
+
+                accum_obj = 0
+
+    def pipeline_execution(self, args={}, save_trajectory=False):
+        self.setup_env(args)
+        self.system_initialization()
+        self.state_predictor_initialization()
+        self.controller_initialization()
+        self.simulator_initialization()
+        self.logger_initialization()
+
+        if self.is_visualization & ~save_trajectory:
+            self.main_loop_visual()
+        else:
+            self.main_loop_raw(save_trajectory)
+
+    def generate_trajectory(self):
+        self.pipeline_execution(save_trajectory=True)
+        with open("./refs/trajectory_2tank.pickle", "wb") as trajectory:
+            pickle.dump(self.trajectory, trajectory)
+
+
 if __name__ == "__main__":
     config_to_pickle("3wrobot")
-    # PresetPipeline3WRobotTest().generate_trajectory()
+    PresetPipeline3WRobotTest().generate_trajectory()
+    config_to_pickle("3wrobot_ni")
     PresetPipeline3WRobotNITest().generate_trajectory()
+    config_to_pickle("2tank")
+    PresetPipeline2TankTest().generate_trajectory()
